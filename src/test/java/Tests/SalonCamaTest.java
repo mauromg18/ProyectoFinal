2715c51dc85e526e4937a46e6107a6aa85dc93bc
Mauro <mauro.smg.1801@gmail.com>
2023-12-04 22:22:02 -0300

    subiendo el test del asiento salon cama

--- src/test/java/Tests/SalonCamaTest.java
+++ src/test/java/Tests/SalonCamaTest.java
@@ -1,7 +1,37 @@
 package Tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import Proyecto.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class SalonCamaTest {
+/**
+ * Clase de prueba para la clase Saloncama.
+ */
+public class SalonCamaTest {
+    private SalonCama salonCama;
 
+    /**
+     * Configuración inicial antes de cada prueba.
+     */
+    @BeforeEach
+    public void setUp() {
+        salonCama = new SalonCama(700);
+    }
+
+    /**
+     * Prueba para obtener el precio del asiento salon cama.
+     */
+    @Test
+    public void testGetPrecio() {
+        Assertions.assertEquals(700, salonCama.getPrecio());
+    }
+
+    /**
+     * Prueba para obtener el tipo de asiento salon cama.
+     */
+    @Test
+    public void testGetTipo() {
+        Assertions.assertEquals("Asiento salon cama", salonCama.getTipo());
+    }
 }
